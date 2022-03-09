be28a11d24f9c71c58db28cf00446e9496050c63
Silvio Traversaro <silvio@traversaro.it>
2022-03-09 08:57:35 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,7 @@ cd build
 
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING=ON \
-      -DPython3_EXECUTABLE:PATH=$PYTHON \
-      -DICUB_MODELS_COMPILE_PYTHON_BINDINGS:BOOL=ON \
-      -DICUB_MODELS_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON \
-      -DICUB_MODELS_PYTHON_PIP_METADATA_INSTALL:BOOL=ON \
-      -DICUB_MODELS_PYTHON_PIP_METADATA_INSTALLER=conda
+      -DBUILD_TESTING=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
